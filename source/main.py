96ec8c6222e31958b00ec647a384a1eb4b1305d3
Sandro <31536101+sandromund@users.noreply.github.com>
2024-07-31 22:56:13 +0200

    Update main.py

--- source/main.py
+++ source/main.py
@@ -1,11 +1,12 @@
 import click
 import yaml
+from icecream import ic
 
 
 def main(config_file_path):
     with open(config_file_path, "r", encoding="utf8") as yaml_file:
         config = yaml.load(yaml_file, Loader=yaml.FullLoader)
-    print(config["demo"]["message"])
+    ic(config["demo"]["message"])
 
 
 @click.group()
@@ -14,12 +15,12 @@ def cli():
 
 
 @click.command()
-@click.option('--config', help='Path to the config file.')
+@click.option("--config", help="Path to the config file.")
 def run(config):
     main(config_file_path=config)
 
 
 cli.add_command(run)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     cli()
